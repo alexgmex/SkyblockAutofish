538491b3847722968ff8d73c554f8d836665ad97
alex-gmex-7 <alex.j.gmex@gmail.com>
2024-04-26 12:23:09 +1000

    refactored, now time for failsafes

--- autofish.py
+++ autofish.py
@@ -1,76 +1,32 @@
+import functions as f
 import cv2 as cv
 import numpy as np
 import pyautogui as pag
 from PIL import ImageGrab
 
-w = 2560
-h = 1440
-minx = round(1/3 * w)
-maxx = round(2/3 * w)
-miny = round(1/3 * h)
-maxy = round(2/3 * h)
 scc = 0.62
 fish_counter = 0
 frames_without_fish = 0
-silent_mode = False
-needle = cv.imread("fishy.png",cv.COLOR_RGB2BGR)
 
-
-def reel(count, conf, time):
-
-    pag.rightClick()
-    cv.waitKey(np.random.randint(500,600))
-
-    if not silent_mode:
-        print(f"Fish Detected! Speed = {time}, Conf = {round(conf,2)}")
-        print(f"{round(count)} Sea Creature(s) Expected")
-
-
-def hype():
-    needle = cv.imread("marker.png",cv.COLOR_RGB2BGR)
-    diff = np.array([0,1000])
-
-    pag.moveTo(None, 5000, 1, pag.easeOutQuad)
-    pag.press('1')
-    for i in range(15):
-        cv.waitKey(np.random.randint(10,50))
-        pag.rightClick()
-    pag.press('3')
-    pag.moveTo(None, 500, 1.5)
-
-    while np.linalg.norm(diff) > 10:
-        frame = pag.screenshot()
-        frame = np.array(frame)
-        frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
-        result = cv.matchTemplate(frame, needle, cv.TM_CCOEFF_NORMED)
-
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-        pos = pag.position()
-        diff[0] = max_loc[0] - pos[0]
-        diff[1] = max_loc[1] - pos[1]
-        
-        pag.move(2*diff[0], 2*diff[1], 0.2)
-
+fish_notif = cv.imread("fishy.png",cv.COLOR_RGB2BGR)
+marker = cv.imread("marker.png",cv.COLOR_RGB2BGR)
 
 while True:
-
-    frame = ImageGrab.grab(bbox = ((minx, miny, maxx, maxy)))
-    frame = np.array(frame)
-    frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
-
+    frame = f.capture(False)
     frames_without_fish += 1
 
-    result = cv.matchTemplate(frame, needle, cv.TM_CCOEFF_NORMED)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    result = cv.matchTemplate(frame, fish_notif, cv.TM_CCOEFF_NORMED)
+    _, max_val, _, _ = cv.minMaxLoc(result)
 
-    if max_val > 0.8 or frames_without_fish > 500:
+    if max_val > 0.8:
         fish_counter += scc
-        reel(fish_counter, max_val,frames_without_fish)
+        f.reel(fish_counter, max_val, frames_without_fish)
         frames_without_fish = 0
 
-        if fish_counter > 7:
+        if fish_counter > 2:
             fish_counter = 0
-            hype()
+            f.hype()
+            f.track_reference(marker)
 
         pag.rightClick()
         
---
+++ functions.py
@@ -0,0 +1,77 @@
+import cv2 as cv
+import numpy as np
+import pyautogui as pag
+from PIL import ImageGrab
+
+# Monitor width and height
+w = 2560
+h = 1440
+
+# How much of screen to capture
+percentage = 0.2
+midpoint_w = round(w/2)
+midpoint_h = round(h/2)
+min_screen_x = round(midpoint_w - percentage*w)
+max_screen_x = round(midpoint_w + percentage*w)
+min_screen_y = round(midpoint_h - percentage*h)
+max_screen_y = round(midpoint_h + percentage*h)
+
+
+
+# Uses a P controller to converge crosshairs to a reference image
+def track_reference(ref_img):
+    
+    # Initialise constant of proportionality, tolerance and error array
+    kp = 1.5
+    tol = 10
+    err = np.array([0,1000])
+
+    # P control towards image while more than tol pixels away
+    while np.linalg.norm(err) > tol:
+            frame = capture(True)
+            result = cv.matchTemplate(frame, ref_img, cv.TM_CCOEFF_NORMED)
+
+            _, _, _, max_loc = cv.minMaxLoc(result)
+            pos = pag.position()
+            err[0] = max_loc[0] - pos[0]
+            err[1] = max_loc[1] - pos[1]
+            
+            pag.move(kp*err[0], kp*err[1], 0.2)
+
+
+
+# Screencapture either entire screen (useful for tracking) or partial screen (useful for fps/reaction time)
+def capture(full):
+
+    if full:
+        frame = ImageGrab.grab()
+    else:  
+        frame = ImageGrab.grab(bbox = ((min_screen_x, min_screen_y, max_screen_x, max_screen_y)))
+
+    frame = np.array(frame)
+    frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+
+    return frame
+
+
+
+# Reel in caught fish and print console message
+def reel(count, conf, time):
+
+    pag.rightClick()
+    cv.waitKey(np.random.randint(500,600))
+    print(f"Fish Detected! Speed = {time}, Conf = {round(conf,2)}")
+    print(f"{round(count)} Sea Creature(s) Expected")
+
+
+
+# Look down to spam hyperion with random timings, then look up
+def hype():
+
+    pag.moveTo(None, 5000, 1, pag.easeOutQuad)
+    pag.press('1')
+    for i in range(20):
+        cv.waitKey(np.random.randint(10,50))
+        pag.rightClick()
+    pag.press('3')
+    pag.moveTo(None, 500, 1.5)
